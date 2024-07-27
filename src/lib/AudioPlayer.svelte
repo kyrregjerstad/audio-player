89ace940b06b89e01107f78b5c3ed5de698eeec7
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-07-27 17:50:25 +0200

    add exclusive prop

--- src/lib/AudioPlayer.svelte
+++ src/lib/AudioPlayer.svelte
@@ -1,3 +1,8 @@
+<script lang="ts" context="module">
+	let globalIsPlaying = $state(false);
+	let globalPlayPause = $state<(() => void)[]>([]);
+</script>
+
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import WaveSurfer, { type WaveSurferOptions } from 'wavesurfer.js';
@@ -7,9 +12,11 @@
 		isPlaying?: boolean;
 		progress?: number;
 		playPause?: () => void;
+		pause?: () => void;
 		stop?: () => void;
 		setVolume?: (volume: number) => void;
 		class?: string;
+		exclusive?: boolean;
 	} & Omit<WaveSurferOptions, 'container' | 'audioprocess' | 'finish' | 'stop'>;
 
 	let {
@@ -19,7 +26,13 @@
 		progress = $bindable(0),
 		playPause = $bindable(),
 		stop = $bindable(),
+		pause = $bindable(() => {
+			if (exclusive && globalIsPlaying) {
+				globalPlayPause.forEach((fn) => fn());
+			}
+		}),
 		setVolume = $bindable(),
+		exclusive = false,
 		...wavesurferRest
 	}: Props = $props();
 
@@ -42,18 +55,47 @@
 		wavesurfer.on('finish', () => {
 			isPlaying = false;
 			progress = 1;
+			if (exclusive) {
+				globalIsPlaying = false;
+			}
 		});
 	});
 
+	pause = () => {
+		wavesurfer?.pause();
+		isPlaying = false;
+		globalPlayPause = globalPlayPause.filter((fn) => fn !== pause);
+		if (globalPlayPause.length === 0) {
+			globalIsPlaying = false;
+		}
+	};
+
 	playPause = () => {
+		if (exclusive && globalIsPlaying && !isPlaying) {
+			globalPlayPause.forEach((fn) => fn());
+		}
+
 		wavesurfer?.playPause();
 		isPlaying = wavesurfer?.isPlaying() || false;
+
+		if (isPlaying) {
+			if (exclusive) {
+				globalPlayPause.push(pause);
+				globalIsPlaying = true;
+			}
+		} else {
+			pause();
+		}
 	};
 
 	stop = () => {
 		wavesurfer?.stop();
 		isPlaying = false;
 		progress = 0;
+		globalPlayPause = globalPlayPause.filter((fn) => fn !== stop);
+		if (globalPlayPause.length === 0) {
+			globalIsPlaying = false;
+		}
 	};
 
 	setVolume = (volume: number) => wavesurfer?.setVolume(volume);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
 	import AudioPlayer from '../lib/AudioPlayer.svelte';
 
-	let playPause = $state(() => {});
-	let isPlaying = $state(false);
-	let progress = $state(0);
+	let playPause1 = $state(() => {});
+	let playPause2 = $state(() => {});
+	let isPlaying1 = $state(false);
+	let isPlaying2 = $state(false);
 </script>
 
 <AudioPlayer
 	src="/audio/voice-sample-1.mp3"
 	barWidth={3}
-	bind:playPause
-	bind:isPlaying
-	bind:progress
+	exclusive={true}
+	bind:playPause={playPause1}
+	bind:isPlaying={isPlaying1}
+/>
+<AudioPlayer
+	src="/audio/voice-sample-1.mp3"
+	barWidth={3}
+	exclusive={true}
+	bind:playPause={playPause2}
+	bind:isPlaying={isPlaying2}
 />
 
-<button onclick={playPause}> {isPlaying ? 'Pause' : 'Play'} </button>
-
-<div>
-	progress = {progress}
-</div>
+<button onclick={playPause1}> {isPlaying1 ? 'Pause' : 'Play'} </button>
+<button onclick={playPause2}> {isPlaying2 ? 'Pause' : 'Play'} </button>
