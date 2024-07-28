32361a59049acf3f4aa2682f78116c1af4dd4642
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-07-28 13:57:09 +0200

    add sample voice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,9 @@
 	let playPause2 = $state(() => {});
 	let isPlaying1 = $state(false);
 	let isPlaying2 = $state(false);
+
+	let progress1 = $state(0);
+	let progress2 = $state(0);
 </script>
 
 <AudioPlayer
@@ -13,6 +16,7 @@
 	exclusive={true}
 	bind:playPause={playPause1}
 	bind:isPlaying={isPlaying1}
+	bind:progress={progress1}
 />
 <AudioPlayer
 	src="/audio/voice-sample-1.mp3"
@@ -20,7 +24,13 @@
 	exclusive={true}
 	bind:playPause={playPause2}
 	bind:isPlaying={isPlaying2}
+	bind:progress={progress2}
 />
 
 <button onclick={playPause1}> {isPlaying1 ? 'Pause' : 'Play'} </button>
 <button onclick={playPause2}> {isPlaying2 ? 'Pause' : 'Play'} </button>
+
+<div>
+	<span>Progress 1: {progress1}</span>
+	<span>Progress 2: {progress2}</span>
+</div>
